ed2bd737bc348c384ae56942f4603a9619e1c4d2
Himkesh Tak <himkeshtak165@gmail.com>
2026-07-12 17:16:45 +0530

    Add: lru-cache - submission-2

---
+++ Data Structures & Algorithms/lru-cache/submission-2.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self, key, val):
+        self.key, self.val = key, val
+        self.prev = self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = {}  # intialized a hashmap for the O(1) time complexity
+
+        #left = LRU , right = most recent
+        self.left, self.right = Node(0,0) , Node(0,0)
+        self.left.next, self.right.prev = self.right , self.left
+    
+    def remove(self, node):
+        prev, nxt = node.prev, node.next # here the node is the bich ka node 
+        prev.next, nxt.prev = nxt, prev # bich wala node ko kiya skip and woh bich ka node hua skip
+
+    def insert(self, node):
+        prev, nxt = self.right.prev, self.right
+        prev.next = node
+        node.prev = prev
+        node.next = nxt
+        nxt.prev = node
+
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        return -1
+        
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+
+        if len(self.cache) > self.cap:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
